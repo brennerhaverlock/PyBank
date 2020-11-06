2f3ff68e772ad3d46df88505deed8ed006c5f456
brennerhaverlock <brenner.haverlock@gmail.com>
2020-11-06 13:53:07 -0500

    init

---
+++ app.py
@@ -0,0 +1,9 @@
+# BankAccount class
+class Bankaccount:
+    def __init__(self):
+      # Function to deposite amount
+
+    def deposit(self):
+        amount = float(input("Enter amount to be deposited: "))
+        self.balance += amount
+        print("\n Amount Deposited:", amount)
